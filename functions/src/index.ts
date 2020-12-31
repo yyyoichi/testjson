f05b3ac37e1a1a561ff2b357a76550b25f21710e
yyyoichi <origamiichiro@gmail.com>
2020-12-31 11:20:07 +0900

    type

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,13 @@
 import * as functions from 'firebase-functions';
+import * as express from "express";
+const app = express();
+
+app.get('/',(req, res)=>{
+  res.send({date: new Date()});
+});
+
+exports.app = functions.https.onRequest(app);
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
